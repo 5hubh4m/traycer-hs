4fa5c55732d404d8beb691337c3442a6389f245e
Shubham Chaudhary <shubhamchaudhary1996@gmail.com>
2018-03-16 19:50:03 +0530

    Fixed example image.

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,28 @@
 module Main where
 
-import Traycer
-import System.Environment
-import Data.Yaml
-import Data.Time
 import Codec.Picture
-import qualified Data.ByteString as B
+import Data.ByteString (readFile)
+import Data.Time
+import Data.Yaml
+import Prelude hiding (readFile)
+import System.Environment
+import Traycer
 
 main :: IO ()
 main = do
   args <- getArgs
-  let (filePath, imageName) = if length args >= 2
-                              then (head args, args !! 1)
-                              else error "Please give atleast two arguments."
-  putStrLn
-    $ "Rendering "
-    ++ filePath
-    ++ " as "
-    ++ imageName
-    ++ "."
-  contents <- B.readFile filePath
-  let config = either error id (decodeEither contents :: Either String (Config Double Int))
+  let (filePath:imageName:_) =
+        case length args of
+          2 -> args
+          x -> error $ "Two arguments expected, " ++ show x ++ " given."
+  putStrLn $ "Rendering " ++ filePath ++ " as " ++ imageName ++ "."
+  contents <- readFile filePath
+  let config =
+        either
+          error
+          id
+          (decodeEither contents :: Either String (Config Double Int))
   start <- getCurrentTime
   savePngImage imageName $ renderImage config
   end <- getCurrentTime
-  putStrLn $ "Finished rendering in "
-           ++ show (end `diffUTCTime` start)
-           ++ "."
-  
+  putStrLn $ "Finished rendering in " ++ show (end `diffUTCTime` start) ++ "."
--- src/Traycer/Geometry/Ray.hs
+++ src/Traycer/Geometry/Ray.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE BangPatterns    #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Traycer.Geometry.Ray
   ( Ray
@@ -12,33 +12,33 @@ module Traycer.Geometry.Ray
   ) where
 
 import Control.Lens
+import Linear.Epsilon
+import Linear.Metric
 import Linear.V3
 import Linear.Vector
-import Linear.Metric
-import Linear.Epsilon
 
-data Ray a = Ray { _origin :: !(V3 a)    -- ^ Origin of the ray
-                 , _direction :: !(V3 a) -- ^ Direction of the ray, must be normalised
-                 , _medium :: !a         -- ^ Refractive index of the medium
-                 }
-           deriving (Show, Eq)
+data Ray a = Ray
+  { _origin :: !(V3 a) -- ^ Origin of the ray
+  , _direction :: !(V3 a) -- ^ Direction of the ray, must be normalised
+  , _medium :: !a -- ^ Refractive index of the medium
+  } deriving (Show, Eq)
 
 makeLenses ''Ray
 
 -- | Make a new 'Ray' by normalising the direction vector
-mkRay :: (Epsilon a, Floating a) => V3 a -> V3 a -> a -> Ray a 
-mkRay !o !d !m = Ray { _origin = o
-                     , _direction = normalize d
-                     , _medium = m
-                     }
+mkRay :: (Epsilon a, Floating a) => V3 a -> V3 a -> a -> Ray a
+mkRay !o !d !m = Ray {_origin = o, _direction = normalize d, _medium = m}
+
 {-# INLINE mkRay #-}
 
 -- | Make a ray going from p1 to p2
 (-->) :: (Epsilon a, Floating a) => V3 a -> V3 a -> a -> Ray a
-(-->) !p1 !p2 = mkRay p1 (p2 - p1) 
+(-->) !p1 !p2 = mkRay p1 (p2 - p1)
+
 {-# INLINE (-->) #-}
 
 -- | Translate the ray 'r's origin by a scalar 't'
 (*->) :: (Num a) => Ray a -> a -> V3 a
-(*->) !r !t = (r^.origin) + t *^ (r^.direction)
+(*->) !r !t = (r ^. origin) + t *^ (r ^. direction)
+
 {-# INLINE (*->) #-}
